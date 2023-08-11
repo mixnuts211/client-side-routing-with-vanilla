82f7466d4038ee1b08dc344f8ee6ecafabb8bd51
MiGyeong Kim <migyeong0211@gmail.com>
2023-08-11 23:20:36 +0900

    feat : reader 페이지 작성

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello");
+function renderHome() {
+  document.querySelector("#app").innerHTML = `
+    <h1>영화를 검색해보세요.</h1>
+    <form>
+      <input type="search" name="query" />
+      <button type="button">검색</button>
+    </form>
+  `;
+}
+
+function renderResult() {
+  document.querySelector("#app").innerHTML = `
+  'search'
+  `;
+}
+
+renderHome();
